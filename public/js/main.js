5c78664dd4a7c4f3006f8117b82622c316b2ba3c
SocialSecurityAdmin <susanlharper01@gmail.com>
2024-03-04 18:54:15 -0800

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+var hour = new Date().getHours()
+var greeting
+if(hour >= 5 && hour < 12){
+    greeting = 'Morning!'
+} else if(hour >= 12 && hour < 18){
+    greeting ='Afternoon!'
+} else {
+    greeting = 'Evening!'
+}
+const greetings = document.querySelector('.greetings')
+greetings.innerHTML = `<h1>Good ${greeting}</h1>`
+
+
+const userName = document.querySelector('#username')
+const password = document.querySelector('#password')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', () => {
+    const obj = {
+        user : userName.value,
+        key : password.value
+    }
+
+    const baseURL = '/formPost'
+    fetch(baseURL, {
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(obj)
+    })
+})
